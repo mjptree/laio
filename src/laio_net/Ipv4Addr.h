021351a7d2fb4f28bc39fd8db7f6309d8485bf21
Michael Prantl <michael.prantl@hotmail.de>
2019-09-25 00:38:31 +0100

    Amend IpAddr implementation method signatures

--- src/laio_net/Ipv4Addr.h
+++ src/laio_net/Ipv4Addr.h
@@ -2,15 +2,62 @@
 #define IPV4ADDR_H
 
 #include <variant>
+#include "IpAddr.h"
 
 namespace laio {
+
+    using std::uint8_t;
 
     template<typename T>
     using Result = std::variant<T, std::exception>;
 
     namespace net {
 
-        class Ipv4Addr {};
+        class Ipv4Addr : public interface::IpAddr {
+
+            IN_ADDR _raw_ipv4_address;
+
+        public:
+
+            explicit constexpr Ipv4Addr(const uint8_t a, const uint8_t b, const uint8_t c, const uint8_t d) noexcept
+                : _raw_ipv4_address{ /* s_addr */ } {}
+
+            /// TODO: To be implemented
+            [[nodiscard]] bool is_unspecified() const noexcept override {
+                return true;
+            }
+
+            /// TODO: To be implemented
+            [[nodiscard]] bool is_loopback() const noexcept override {
+                return true;
+            }
+
+            /// TODO: To be implemented
+            [[nodiscard]] bool is_global() const noexcept override {
+                return true;
+            }
+
+            /// TODO: To be implemented
+            [[nodiscard]] bool is_multicast() const noexcept override {
+                return true;
+            }
+
+            /// TODO: To be implemented
+            [[nodiscard]] bool is_documentation() const noexcept override {
+                return true;
+            }
+
+            /// Return `true`, as this IP address is implement as IPv4
+            [[nodiscard]] bool is_ipv4() const noexcept override {
+                return true;
+            }
+
+            /// Return `false`, as this IP address is implement as IPv4
+            [[nodiscard]] bool is_ipv6() const noexcept override {
+                return false;
+            }
+
+        };
 
     } // namespace net
 
--- src/laio_net/Ipv6Addr.h
+++ src/laio_net/Ipv6Addr.h
@@ -1,7 +1,10 @@
 #ifndef IPV6ADDR_H
 #define IPV6ADDR_H
 
+#include <WS2tcpip.h>
+
 #include <variant>
+#include "IpAddr.h"
 
 namespace laio {
 
@@ -10,7 +13,51 @@ namespace laio {
 
     namespace net {
 
-        class Ipv6Addr {};
+        class Ipv6Addr : public interface::IpAddr {
+
+            IN6_ADDR _raw_ipv6_address;
+
+        public:
+
+            explicit constexpr Ipv6Addr() noexcept
+                : _raw_ipv6_address{} {}
+
+            /// TODO: To be implemented
+            [[nodiscard]] bool is_unspecified() const noexcept override {
+                    return true;
+            }
+
+            /// TODO: To be implemented
+            [[nodiscard]] bool is_loopback() const noexcept override {
+                    return true;
+            }
+
+            /// TODO: To be implemented
+            [[nodiscard]] bool is_global() const noexcept override {
+                    return true;
+            }
+
+            /// TODO: To be implemented
+            [[nodiscard]] bool is_multicast() const noexcept override {
+                    return true;
+            }
+
+            /// TODO: To be implemented
+            [[nodiscard]] bool is_documentation() const noexcept override {
+                    return true;
+            }
+
+            /// Return `false`, as this IP address is implement as IPv6
+            [[nodiscard]] bool is_ipv4() const noexcept override {
+                    return false;
+            }
+
+            /// Return `true`, as this IP address is implement as IPv6
+            [[nodiscard]] bool is_ipv6() const noexcept override {
+                    return true;
+            }
+
+        };
 
     } // namespace net
 
