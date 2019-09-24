cc9f8478ab3da9366ae59ffdb9c04823805b3b12
Michael Prantl <michael.prantl@hotmail.de>
2019-09-25 00:36:45 +0100

    Add docstrings to IpAddr interface

--- src/laio_net/interfaces/IpAddr.h
+++ src/laio_net/interfaces/IpAddr.h
@@ -1,7 +1,14 @@
 #ifndef IPADDR_H
 #define IPADDR_H
 
+#include <WinSock2.h>
+
 #include <variant>
+
+// Symbol defined as __STRUCT__ in <combaseapi.h>
+#ifdef interface
+    #undef interface
+#endif
 
 namespace laio {
 
@@ -18,25 +25,25 @@ namespace laio {
         struct IpAddr {
 
             /// Return `true` if IpAddr is set to `unspecified` address
-            virtual bool is_unspecified() = 0;
+            [[nodiscard]] virtual bool is_unspecified() const noexcept = 0;
 
             /// Return `true` if IpAddr is set to a loopback address
-            virtual bool is_loopback() = 0;
+            [[nodiscard]] virtual bool is_loopback() const noexcept = 0;
 
             /// Return `true` if IpAddr is within a globally routable range
-            virtual bool is_global() = 0;
+            [[nodiscard]] virtual bool is_global() const noexcept = 0;
 
             /// Return `true` if IpAddr is set to a multicast address
-            virtual bool is_multicast() = 0;
+            [[nodiscard]] virtual bool is_multicast() const noexcept = 0;
 
             /// Return `true` if IpAddr is within a range designated for documentation
-            virtual bool is_documentation() = 0;
+            [[nodiscard]] virtual bool is_documentation() const noexcept = 0;
 
             /// Returns `true` if IpAddr is implemented as an Ipv4Addr
-            virtual bool is_ipv4() = 0;
+            [[nodiscard]] virtual bool is_ipv4() const noexcept = 0;
 
             /// Returns `true` if IpAddr is implemented as an Ipv6Addr
-            virtual bool is_ipv6() = 0;
+            [[nodiscard]] virtual bool is_ipv6() const noexcept = 0;
 
             virtual ~IpAddr() = default;
 
