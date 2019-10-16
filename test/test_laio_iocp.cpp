5f75de83e6f1c59eeae842914818a23f65ff2998
Michael Prantl <michael.prantl@hotmail.de>
2019-10-17 00:14:38 +0100

    Complete test cases for CompletionStatus

--- test/test_laio_iocp.cpp
+++ test/test_laio_iocp.cpp
@@ -1,7 +1,7 @@
 #include "catch2/catch.hpp"
 
 #include <chrono>
-#include <iostream>
+#include <vector>
 #include "CompletionPort.h"
 #include "CompletionStatus.h"
 #include "Handle.h"
@@ -13,6 +13,12 @@ TEST_CASE("CompletionPort") {
     // CompletionPort has traits `is_send` and `is_sync`
     CHECK(laio::trait::is_send<CompletionPort>);
     CHECK(laio::trait::is_sync<CompletionPort>);
+
+    // CompletionPort also has the traits `as_raw_handle`, `from_raw_handle` and
+    // `into_raw_handle`
+    CHECK(laio::trait::as_raw_handle<CompletionPort>);
+    CHECK(laio::trait::from_raw_handle<CompletionPort>);
+    CHECK(laio::trait::into_raw_handle<CompletionPort>);
 
     // Associating a new handle or socket to the completion port the tokens must
     // have the same size in memory as `ULONG_PTR`
@@ -32,32 +38,44 @@ TEST_CASE("CompletionStatus") {
     CHECK(laio::trait::is_sync<CompletionStatus>);
 
     // `get` dequeues a CompletionStatus from the associated port
-
+    CompletionPort port1 = std::get<CompletionPort>(CompletionPort::create(1));
+    Overlapped async1 = Overlapped::zero();
+    port1.post(CompletionStatus::create(1, 2, &async1));
+    CompletionStatus message = std::get<CompletionStatus>(port1.get(std::nullopt));
+    CHECK(message.bytes_transferred() == 1);
+    CHECK(message.token() == 2);
+    CHECK(message.overlapped() == async1.raw());
 
     // So does `get_many`, which must be provided with an array of zeroed
     // CompletionStatuses
-    CompletionPort port = std::get<CompletionPort>(CompletionPort::create(1));
-    Overlapped async1 = Overlapped::zero();
+    CompletionPort port2 = std::get<CompletionPort>(CompletionPort::create(1));
     Overlapped async2 = Overlapped::zero();
-    port.post(CompletionStatus::create(1, 2, &async1));
-    port.post(CompletionStatus::create(4, 5, &async2));
+    Overlapped async3 = Overlapped::zero();
+    port2.post(CompletionStatus::create(1, 2, &async2));
+    port2.post(CompletionStatus::create(4, 5, &async3));
     std::vector<CompletionStatus> messageQueue(4, CompletionStatus::zero());
-    {
-        // View into the message queue
-        gsl::span<CompletionStatus> dequeued = std::get<0>(port.get_many(messageQueue, std::nullopt));
-        CHECK(dequeued.size() == 2);
 
-        // First dequeued message
-        CHECK(dequeued[0].bytes_transferred() == 1);
-        CHECK(dequeued[0].token() == 2);
-        /* CHECK(dequeued[0].overlapped() == async1); */
+    // View into the message queue
+    gsl::span<CompletionStatus> dequeued = std::get<0>(port2.get_many(messageQueue, std::nullopt));
+    CHECK(dequeued.size() == 2);
+    auto dequeuedIndex = dequeued.begin();
 
-        // Second dequeued message
-        CHECK(dequeued[1].bytes_transferred() == 4);
-        CHECK(dequeued[1].token() == 5);
-        /* CHECK(dequeued[1].overlapped() == async2); */
-    }
-    // Remaining message
+    // First dequeued message
+    CHECK(dequeuedIndex->bytes_transferred() == 1);
+    CHECK(dequeuedIndex->token() == 2);
+    CHECK(dequeuedIndex->overlapped() == async2.raw());
+
+    // Second dequeued message
+    std::advance(dequeuedIndex, 1);
+    CHECK(dequeuedIndex->bytes_transferred() == 4);
+    CHECK(dequeuedIndex->token() == 5);
+    CHECK(dequeuedIndex->overlapped() == async3.raw());
+
+    // No more messages dequeued
+    std::advance(dequeuedIndex, 1);
+    CHECK(dequeuedIndex == dequeued.end());
+
+    // Remaining messages in the target array are still zeroed
     CHECK(messageQueue[2].bytes_transferred() == 0);
     CHECK(messageQueue[2].token() == 0);
     CHECK(messageQueue[2].overlapped() == nullptr);
