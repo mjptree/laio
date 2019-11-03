cdd5f47d9db72384e9e85b1a28752fcbc3d75d34
Michael Prantl <michael.prantl@hotmail.de>
2019-11-03 01:21:36 +0000

    Implement IPv4 address class

--- src/laio_net/Ipv4Addr.h
+++ src/laio_net/Ipv4Addr.h
@@ -1,64 +1,281 @@
+#pragma clang diagnostic push
+#pragma ide diagnostic ignored "hicpp-move-const-arg"
+#pragma ide diagnostic ignored "bugprone-branch-clone"
+#pragma ide diagnostic ignored "HidingNonVirtualFunction"
 #pragma once
 
+#include <WinSock2.h>
+
+#include <cstdint>
+#include <tuple>
 #include <variant>
 
 #include "IpAddr.h"
+#include "Ipv6Addr.h"
 
 namespace laio {
 
     using std::uint8_t;
+    using std::uint32_t;
 
     template<typename T>
     using Result = std::variant<T, std::exception>;
 
     namespace net {
 
-        class Ipv4Addr final : public interface::IpAddr {
+        ///
+        ///
+        /// \details
+        class Ipv4Addr final : public interface::IpAddr<Ipv4Addr> {
 
-            IN_ADDR raw_ipv4_address_;
+            IN_ADDR raw_ipv4_address_{};        ///< Windows IpV4 address structure (unsafe union access!)
 
         public:
+            // # Constructors
+            constexpr Ipv4Addr() noexcept = default;
+
+            constexpr Ipv4Addr(IN_ADDR ipv4Address) noexcept // NOLINT(google-explicit-constructor,hicpp-explicit-conversions)
+                : raw_ipv4_address_{std::move(ipv4Address)} {}
 
             explicit constexpr Ipv4Addr(const uint8_t a, const uint8_t b, const uint8_t c, const uint8_t d) noexcept
-                : raw_ipv4_address_{ /* s_addr */ } {}
+                : raw_ipv4_address_{ { {a, b, c, d} /* struct */ } /* union */ } {}
 
-            /// TODO: To be implemented
-            [[nodiscard]] bool is_unspecified() const noexcept override {
+            // # Operator overloads
+            constexpr operator IN_ADDR() const noexcept { // NOLINT(google-explicit-constructor,hicpp-explicit-conversions)
+                return raw_ipv4_address_;
+            }
+
+            bool operator==(const Ipv4Addr& rhs) const noexcept {
+                return raw_ipv4_address_.S_un.S_addr == rhs.raw_ipv4_address_.S_un.S_addr;
+            }
+
+            bool operator<(const Ipv4Addr& rhs) const noexcept {
+                auto [lhs_a, lhs_b, lhs_c, lhs_d] = octets();
+                auto [rhs_a, rhs_b, rhs_c, rhs_d] = rhs.octets();
+                return lhs_a <= rhs_b
+                    && lhs_b <= rhs_b
+                    && lhs_c <= rhs_c
+                    && lhs_d < rhs_d;
+            }
+
+            bool operator==(const Ipv6Addr& rhs) const noexcept {
+                return false;
+            }
+
+            bool operator<(const Ipv6Addr& rhs) const noexcept {
                 return true;
             }
 
-            /// TODO: To be implemented
-            [[nodiscard]] bool is_loopback() const noexcept override {
-                return true;
+            // # Public member functions
+
+            ///
+            ///
+            /// \return
+            [[nodiscard]] constexpr std::tuple<uint8_t, uint8_t, uint8_t,  uint8_t> octets() const noexcept {
+                uint8_t a = raw_ipv4_address_.S_un.S_un_b.s_b1;
+                uint8_t b = raw_ipv4_address_.S_un.S_un_b.s_b2;
+                uint8_t c = raw_ipv4_address_.S_un.S_un_b.s_b3;
+                uint8_t d = raw_ipv4_address_.S_un.S_un_b.s_b4;
+                return {a, b, c, d};
             }
 
-            /// TODO: To be implemented
-            [[nodiscard]] bool is_global() const noexcept override {
-                return true;
+            ///
+            /// \return
+            [[nodiscard]] constexpr bool is_unspecified() const noexcept {
+                return raw_ipv4_address_.S_un.S_addr == 0x00'00'00'00;
             }
 
-            /// TODO: To be implemented
-            [[nodiscard]] bool is_multicast() const noexcept override {
-                return true;
+            ///
+            ///
+            /// \details
+            ///
+            /// \return
+            [[nodiscard]] constexpr bool is_loopback() const noexcept {
+                return raw_ipv4_address_.S_un.S_un_b.s_b1 == 0x7f;
             }
 
-            /// TODO: To be implemented
-            [[nodiscard]] bool is_documentation() const noexcept override {
-                return true;
+            ///
+            ///
+            /// \details
+            ///
+            /// \return
+            [[nodiscard]] constexpr bool is_private() const noexcept {
+
+                // The last two bytes are not of interest
+                auto [a, b, c_, d_] = octets();
+                switch (a) {
+                    case 10 : return true;
+                    case 172: return b >= 16 && b <= 31;
+                    case 192: return b == 168;
+                    default : return false;
+                }
+            }
+
+            ///
+            ///
+            /// \details
+            ///
+            /// \return
+            [[nodiscard]] constexpr bool is_link_local() const noexcept {
+
+                // The last two bytes are not of interest
+                auto [a, b, c_, d_] = octets();
+                return a == 169 && b == 254;
+            }
+
+            ///
+            ///
+            /// \details
+            ///
+            /// \return
+            [[nodiscard]] constexpr bool is_broadcast() const noexcept {
+                return raw_ipv4_address_.S_un.S_addr == 0xff'ff'ff'ff;
+            }
+
+            ///
+            ///
+            /// \details
+            ///
+            /// \return
+            [[nodiscard]] constexpr bool is_documentation() const noexcept {
+                switch (raw_ipv4_address_.S_un.S_addr & 0x00'ff'ff'ffu) {
+                    case 0x00'02'00'c0: return true;
+                    case 0x00'64'33'c6: return true;
+                    case 0x00'71'00'cb: return true;
+                    default: return false;
+                }
+            }
+
+            ///
+            ///
+            /// \details
+            ///
+            /// \return
+            [[nodiscard]] constexpr bool is_shared() const noexcept {
+                return raw_ipv4_address_.S_un.S_un_b.s_b1 == 100
+                    && (raw_ipv4_address_.S_un.S_un_b.s_b2 & 0b1100'0000u) == 0b0100'0000u;
+            }
+
+            ///
+            ///
+            /// \details
+            ///
+            /// \return
+            [[nodiscard]] constexpr bool is_ietf_protocol_assignment() const noexcept {
+                auto [a, b, c, d_] = octets();
+                return a == 192 && b == 0 && c == 0;
+            }
+
+            ///
+            ///
+            /// \details
+            ///
+            /// \return
+            [[nodiscard]] constexpr bool is_reserved() const noexcept {
+                return (raw_ipv4_address_.S_un.S_un_b.s_b1 & 240u) == 240 && !is_broadcast();
+            }
+
+            ///
+            ///
+            /// \details
+            ///
+            /// \return
+            [[nodiscard]] constexpr bool is_benchmarking() const noexcept {
+                return raw_ipv4_address_.S_un.S_un_b.s_b1 == 198
+                       && (raw_ipv4_address_.S_un.S_un_b.s_b2 & 0xfeu) == 18;
+            }
+
+            ///
+            ///
+            /// \details
+            ///
+            /// \return
+            [[nodiscard]] constexpr bool is_global() const noexcept {
+
+                // Only 192.0.0.9 and 192.0.0.10 are globally routable in 192.0.0.0/24
+                if (raw_ipv4_address_.S_un.S_addr == 0x09'00'00'c0 || raw_ipv4_address_.S_un.S_addr == 0x0a'00'00'c0)
+                    return true;
+                return !is_private()
+                    && !is_loopback()
+                    && !is_link_local()
+                    && !is_broadcast()
+                    && !is_documentation()
+                    && !is_shared()
+                    && !is_ietf_protocol_assignment()
+                    && !is_reserved()
+                    && !is_benchmarking()
+                    && raw_ipv4_address_.S_un.S_un_b.s_b1 != 0;
+            }
+
+            ///
+            ///
+            /// \details
+            ///
+            /// \return
+            [[nodiscard]] constexpr bool is_multicast() const noexcept {
+                uint8_t a = raw_ipv4_address_.S_un.S_un_b.s_b1;
+                return a >= 224 && a <= 239;
+            }
+
+            ///
+            ///
+            /// \details
+            ///
+            /// \return
+            [[nodiscard]] Ipv6Addr to_ipv6_compatible() const noexcept {
+                auto [a, b, c, d] = octets();
+                return Ipv6Addr{0, 0, 0, 0,
+                                0, 0, 0, 0,
+                                0, 0, 0, 0,
+                                a, b, c, d};
+            }
+
+            ///
+            ///
+            /// \details
+            ///
+            /// \return
+            [[nodiscard]] Ipv6Addr to_ipv6_mapped() const noexcept {
+                auto [a, b, c, d] = octets();
+                return Ipv6Addr{0, 0, 0, 0,
+                                0, 0, 0, 0,
+                                0, 0, 0xFF, 0xFF,
+                                a, b, c, d};
             }
 
             /// Return `true`, as this IP address is implemented as IPv4
-            [[nodiscard]] bool is_ipv4() const noexcept override {
+            [[nodiscard]] constexpr bool is_ipv4() const noexcept {
                 return true;
             }
 
             /// Return `false`, as this IP address is implemented as IPv4
-            [[nodiscard]] bool is_ipv6() const noexcept override {
+            [[nodiscard]] constexpr bool is_ipv6() const noexcept {
                 return false;
             }
 
         };
 
+        std::optional<Ipv4Addr> Ipv6Addr::to_ipv4() const noexcept {
+            auto [a, b, c, d, e, f, g, h] = segments();
+            if (a == 0 && b == 0 && c == 0 && d == 0 && e == 0 && (f == 0 || f == 0xff'ff))
+                return Ipv4Addr{static_cast<uint8_t>(g >> 8u),
+                                static_cast<uint8_t>(g),
+                                static_cast<uint8_t>(h >> 8u),
+                                static_cast<uint8_t>(h)};
+            return std::nullopt;
+        }
+
+        namespace ipv4 {
+
+            const Ipv4Addr LOCALHOST{127, 0, 0, 1};         ///< IPv4 loopback address
+
+            const Ipv4Addr UNSPECIFIED{0, 0, 0, 0};         ///< IPv4 unspecified address
+
+            const Ipv4Addr BROADCAST{255, 255, 255, 255};   ///< IPv4 broadcast address
+
+        } // namespace ipv4
+
     } // namespace net
 
 } // namespace laio
+#pragma clang diagnostic pop
--- test/test_laio_net.cpp
+++ test/test_laio_net.cpp
@@ -1,9 +1,166 @@
 #include "catch2/catch.hpp"
 
-TEST_CASE() {
-    
+#include "Ipv4Addr.h"
+#include "Ipv6Addr.h"
+
+TEST_CASE("Ipv4Addr") {
+    using namespace laio::net;
+
+    // Localhost address space: 127.0.0.0/8
+    Ipv4Addr localhost{127, 0, 0, 1};
+    Ipv4Addr not_localhost{45, 22, 13, 197};
+    CHECK(localhost.is_loopback());
+    CHECK(localhost == ipv4::LOCALHOST);
+    CHECK_FALSE(not_localhost.is_loopback());
+    CHECK_FALSE(not_localhost == ipv4::LOCALHOST);
+
+    // Octets can be retrieved in correct order as a tuple
+    std::tuple<uint8_t, uint8_t, uint8_t, uint8_t> octets{127, 0, 0, 1};
+    CHECK(localhost.octets() == octets);
+
+    // Unspecified address: 0.0.0.0
+    Ipv4Addr unspecified{0, 0, 0, 0};
+    Ipv4Addr not_unspecified{45, 22, 13, 197};
+    CHECK(unspecified.is_unspecified());
+    CHECK(unspecified == ipv4::UNSPECIFIED);
+    CHECK_FALSE(not_unspecified.is_unspecified());
+    CHECK_FALSE(not_unspecified == ipv4::UNSPECIFIED);
+
+    // Broadcast address: 255.255.255.255
+    Ipv4Addr broadcast{255, 255, 255, 255};
+    Ipv4Addr not_broadcast{236, 168, 10, 65};
+    CHECK(broadcast.is_broadcast());
+    CHECK(broadcast == ipv4::BROADCAST);
+    CHECK_FALSE(not_broadcast.is_broadcast());
+    CHECK_FALSE(not_broadcast == ipv4::BROADCAST);
+
+    // Private address space: 10.0.0.0/8
+    //                        172.16.0.0/12
+    //                        192.168.0.0/16
+    CHECK(Ipv4Addr{10, 0, 0, 1}.is_private());
+    CHECK(Ipv4Addr{10, 10, 10, 10}.is_private());
+    CHECK(Ipv4Addr{172, 16, 20, 10}.is_private());
+    CHECK(Ipv4Addr{172, 29, 45, 14}.is_private());
+    CHECK(Ipv4Addr{192, 168, 0, 2}.is_private());
+    CHECK_FALSE(Ipv4Addr{172, 32, 0, 2}.is_private());
+    CHECK_FALSE(Ipv4Addr{192, 169, 0, 2}.is_private());
+
+    // Link-local address space: 169.254.0.0/16
+    CHECK(Ipv4Addr{169, 254, 0, 0}.is_link_local());
+    CHECK(Ipv4Addr{169, 254, 10, 65}.is_link_local());
+    CHECK_FALSE(Ipv4Addr{16, 89, 10, 65}.is_link_local());
+
+    // Globally routable addresses are...
+    // ... not private
+    CHECK_FALSE(Ipv4Addr{10, 254, 0, 0}.is_global());
+    CHECK_FALSE(Ipv4Addr{192, 168, 10, 65}.is_global());
+    CHECK_FALSE(Ipv4Addr{172, 16, 10, 65}.is_global());
+
+    // ... not unspecified or 0.0.0.0/8
+    CHECK_FALSE(Ipv4Addr{0, 1, 2, 3}.is_global());
+    CHECK_FALSE(Ipv4Addr{0, 0, 0, 0}.is_global());
+    CHECK_FALSE(ipv4::UNSPECIFIED.is_global());
+
+    // ... not local
+    CHECK_FALSE(Ipv4Addr{127, 0, 0, 1}.is_global());
+    CHECK_FALSE(ipv4::LOCALHOST.is_global());
+
+    // ... not broadcast
+    CHECK_FALSE(Ipv4Addr{255, 255, 255, 255}.is_global());
+    CHECK_FALSE(ipv4::BROADCAST.is_global());
+
+    // ... not designated for documentation
+    CHECK_FALSE(Ipv4Addr{192, 0, 2, 255}.is_global());
+    CHECK_FALSE(Ipv4Addr{198, 51, 100, 65}.is_global());
+    CHECK_FALSE(Ipv4Addr{203, 0, 113, 6}.is_global());
+
+    // ... not shared
+    CHECK_FALSE(Ipv4Addr{100, 100, 0, 0}.is_global());
+
+    // ... not reserved for protocol assignment
+    CHECK_FALSE(Ipv4Addr{192, 0, 0, 0}.is_global());
+    CHECK_FALSE(Ipv4Addr{192, 0, 0, 255}.is_global());
+
+    // ... not reserved for future use
+    CHECK_FALSE(Ipv4Addr{250, 10, 20, 30}.is_global());
+
+    // ... not reserved for network device benchmarking
+    CHECK_FALSE(Ipv4Addr{198, 18, 0, 0}.is_global());
+
+    // ... all other addresses
+    CHECK(Ipv4Addr{1, 1, 1, 1}.is_global());
+    CHECK(Ipv4Addr{80, 9, 12, 3}.is_global());
+    CHECK(Ipv4Addr{192, 0, 0, 9}.is_global());
+    CHECK(Ipv4Addr{192, 0, 0, 10}.is_global());
+
+    // Shared address space: 100.64.0.0/10
+    CHECK(Ipv4Addr{100, 64, 0, 0}.is_shared());
+    CHECK(Ipv4Addr{100, 127, 255, 255}.is_shared());
+    CHECK_FALSE(Ipv4Addr{100, 128, 0, 0}.is_shared());
+
+    // Address space reserved by IANA for IETF protocol assignment: 192.0.0.0/24
+    CHECK(Ipv4Addr{192, 0, 0, 0}.is_ietf_protocol_assignment());
+    CHECK(Ipv4Addr{192, 0, 0, 8}.is_ietf_protocol_assignment());
+    CHECK(Ipv4Addr{192, 0, 0, 9}.is_ietf_protocol_assignment());
+    CHECK(Ipv4Addr{192, 0, 0, 255}.is_ietf_protocol_assignment());
+    CHECK_FALSE(Ipv4Addr{192, 0, 1, 0}.is_ietf_protocol_assignment());
+    CHECK_FALSE(Ipv4Addr{191, 255, 255, 255}.is_ietf_protocol_assignment());
+
+    // Address space for network device benchmarking: 198.18.0.0/15
+    CHECK_FALSE(Ipv4Addr{198, 17, 255, 255}.is_benchmarking());
+    CHECK(Ipv4Addr{198, 18, 0, 0}.is_benchmarking());
+    CHECK(Ipv4Addr{198, 19, 255, 255}.is_benchmarking());
+
+    // Address space reserved by IANA for future use
+    CHECK(Ipv4Addr{240, 0, 0, 0}.is_reserved());
+    CHECK(Ipv4Addr{255, 255, 255, 254}.is_reserved());
+    CHECK_FALSE(Ipv4Addr{239, 255, 255, 255}.is_reserved());
+    CHECK_FALSE(Ipv4Addr{255, 255, 255, 255}.is_reserved());
+
+    // Multicast address space: 224.0.0./4
+    CHECK(Ipv4Addr{224, 254, 0, 0}.is_multicast());
+    CHECK(Ipv4Addr{236, 168, 10, 65}.is_multicast());
+    CHECK_FALSE(Ipv4Addr{172, 16, 10, 65}.is_multicast());
+
+    // Address space designated for documentation: 192.0.2.0/24
+    //                                             198.51.100.0/24
+    //                                             203.0.113.0/24
+    CHECK(Ipv4Addr{192, 0, 2, 255}.is_documentation());
+    CHECK(Ipv4Addr{198, 51, 100, 65}.is_documentation());
+    CHECK(Ipv4Addr{203, 0, 113, 6}.is_documentation());
+    CHECK_FALSE(Ipv4Addr{193, 34, 17, 19}.is_documentation());
+
+    // Convert IPv4 to an IPv4-compatible IPv6 address
+    Ipv6Addr ipv4_compatible = Ipv4Addr{192, 0, 2, 255}.to_ipv6_compatible();
+    CHECK(ipv4_compatible == Ipv6Addr{0, 0, 0, 0, 0, 0, 49152, 767});
+
+    // Convert IPv4 to an IPv4-mapped IPv6 address
+    Ipv6Addr ipv4_mapped = Ipv4Addr{192, 0, 2, 255}.to_ipv6_mapped();
+    CHECK(ipv4_mapped == Ipv6Addr{0, 0, 0, 0, 0, 65535, 49152, 767});
 }
 
-TEST_CASE() {
+TEST_CASE("Ipv6Addr") {
+    using namespace laio::net;
 
+    // Localhost address: `::1`
+    Ipv6Addr localhost{0, 0, 0, 0, 0, 0, 0, 1};
+    Ipv6Addr not_localhost{0, 0, 0, 0, 0, 0xff'ff, 0xc0'0a, 0x02'ff};
+    CHECK(localhost.is_loopback());
+    CHECK(localhost == ipv6::LOCALHOST);
+    CHECK_FALSE(not_localhost.is_loopback());
+    CHECK_FALSE(not_localhost == ipv6::LOCALHOST);
+
+    // Unspecified address: `::`
+    Ipv6Addr unspecified{0, 0, 0, 0, 0, 0, 0, 0};
+    Ipv6Addr not_unspecified{0, 0, 0, 0, 0, 0xff'ff, 0xc0'0a, 0x02'ff};
+    CHECK(unspecified.is_unspecified());
+    CHECK(unspecified == ipv6::UNSPECIFIED);
+    CHECK_FALSE(not_unspecified.is_unspecified());
+    CHECK_FALSE(not_unspecified == ipv6::UNSPECIFIED);
+
+    // Segments can be retrieved in as a tuple
+    Ipv6Addr mapped_ipv4 = Ipv4Addr{192, 0, 2, 255}.to_ipv6_mapped();
+    std::tuple<uint16_t, uint16_t, uint16_t, uint16_t,
+               uint16_t, uint16_t, uint16_t,  uint16_t> segments{0, 0, 0, 0, 0, 65535, 49152, 767};
+    CHECK(mapped_ipv4.segments() == segments);
 }
